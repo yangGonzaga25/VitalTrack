982d5d4edd3f3a4ebf1b358f97af9e6ec95680ef
Tanya Grace S. Gonzaga <tanyangqty25@gmail.com>
2025-03-27 18:11:02 +0800

    Activity4

--- VitalTrack/settings.py
+++ VitalTrack/settings.py
@@ -76,10 +76,15 @@ WSGI_APPLICATION = 'VitalTrack.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sampledb',  
+        'USER': 'USER',      
+        'PASSWORD': '123',   
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Students(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    age = models.IntegerField()
+    email = models.EmailField(unique=True)
+    grade = models.CharField(max_length=10) 
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
